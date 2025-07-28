d532b57731eede09c50d9f9f360cb5df3900c045
PiezoGo <piezo5go@gmail.com>
2025-07-28 15:34:03 +0300

    with return

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -20,10 +20,12 @@ def stream_users_in_batches(batch_size):
 
     cursor.close()
     conn.close()
+    return  # <-- Proper use of return in a generator (to end it)
 
 
 def batch_processing(batch_size):
     for batch in stream_users_in_batches(batch_size):  # 1st loop
-        filtered_batch = [user for user in batch if user["age"] > 25]  # 2nd loop (list comprehension)
+        filtered_batch = [user for user in batch if user["age"] > 25]  # 2nd loop
         for user in filtered_batch:  # 3rd loop
             yield user
+    return  # <-- Ends generator explicitly
