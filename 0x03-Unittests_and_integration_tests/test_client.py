4d0077524192e9283bd1a5e2564b0fdc7f957fd6
PiezoGo <piezo5go@gmail.com>
2025-08-14 08:15:30 +0300

    task 7 completetd

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -73,6 +73,18 @@ class TestGithubOrgClient(unittest.TestCase):
             "https://api.github.com/orgs/test/repos"
         )
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """
+        Test that has_license returns True if repo has the license_key,
+        otherwise returns False.
+        """
+        client = GithubOrgClient("test")
+        self.assertEqual(client.has_license(repo, license_key), expected)
+
 
 if __name__ == "__main__":
     unittest.main()
